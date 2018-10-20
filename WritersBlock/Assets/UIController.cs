89b99502012f8dee0baab63cecf7843cbd7e98d1
David Smith <dms92@protonmail.ch>
2018-10-20 19:44:01 -0400

    more work on book level 1

--- WritersBlock/Assets/UIController.cs
+++ WritersBlock/Assets/UIController.cs
@@ -7,11 +7,15 @@ using UnityEngine.UI;
 public class UIController : MonoBehaviour {
 
 	public Animator _CamAnimator;
+    public GameObject startB;
+    public GameObject aboutB;
+    public GameObject quitB;
+    public GameObject flipB;
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -19,13 +23,16 @@ public class UIController : MonoBehaviour {
 	}
 
 	public void StartGame(){
-		_CamAnimator.SetTrigger ("GameStartZoom");
-		var bkCtrl = GameObject.FindObjectOfType<MegaBookBuilder>();
-        Texture2D tex = new Texture2D(1024, 1024);
-        //bkCtrl.FetchTexture("https://images.pexels.com/photos/259915/pexels-photo-259915.jpeg", tex);
-        bkCtrl.SetPageTexture(LoadPNG("C:\\Users\\David\\Documents\\WritersBlock\\WritersBlock\\Images\\prototype_background.jpg"), 0, false);
-        bkCtrl.NextPage ();
-		//bkCtrl.NextPage ();
+        Object.Destroy(startB);
+        Object.Destroy(aboutB);
+        Object.Destroy(quitB);
+        Object.Destroy(flipB);
+        var bkCtrl = GameObject.FindObjectOfType<MegaBookBuilder>();
+        bkCtrl.SetPageTexture(LoadPNG("C:\\Users\\David\\Documents\\WritersBlock\\WritersBlock\\Images\\prototype_background_L.jpg"), 0, false);
+        bkCtrl.SetPageTexture(LoadPNG("C:\\Users\\David\\Documents\\WritersBlock\\WritersBlock\\Images\\prototype_background_R.jpg"), 1, true);
+        _CamAnimator.SetTrigger ("GameStartZoom");
+        bkCtrl.NextPage();
+        bkCtrl.NextPage();
 	}
 
     public static Texture2D LoadPNG(string filePath)
